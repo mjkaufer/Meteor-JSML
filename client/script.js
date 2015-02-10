684b0ec7feb58343185d9e26fa8404c913c5b687
Matthew Kaufer <mjkaufer@gmail.com>
2015-02-10 20:03:20 +0000

    autofocus

--- client/script.js
+++ client/script.js
@@ -4,6 +4,11 @@ var compile = function(){
     Meteor.call("compile",$('#in').val(),function(e,r){console.log(r); $('#out').val(r)})
 }
 
+Template.fields.rendered = function(){
+    $('#in').focus();
+}
+
 $('#compile').click(function(){
     compile();
 });
+
